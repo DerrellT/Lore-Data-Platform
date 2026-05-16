bc5bd9a096886d411831d7ac377856b810b1e9cb
Derrell Turner <derrellturner@Mac.attlocal.net>
2026-05-16 11:33:23 -0500

    Add regions dataset and link characters to regions with search support

--- main.py
+++ main.py
@@ -3,67 +3,88 @@ import json
 
 #Loads character dataset from JSON file and returns parsed data.
 def check_json_files():
-    chars = None
+    lore_data = None
     try :
-        with open('data/test.json') as f:
-            chars = json.load(f)
-            return chars
+        with open('data/lore_data.json') as f:
+            lore_data = json.load(f)
+            return lore_data
     except FileNotFoundError:
         print("Missing character")
         return None
 
 #Searches character dataset for a matching name and returns the character if found.
-def search_characters(name, chars):
-    for character in chars["characters"]:
+def search_characters(name, lore_data):
+    for character in lore_data["characters"]:
         if character["name"].lower() == name:
             return character 
+    return None
+
+def search_region(name, lore_data):
+    for region in lore_data["regions"]:
+        if region["name"].lower() == name:
+            return region 
     return None
 
 #Formats and displays character information to the user.
 def display_character(character): 
     print(f"Name: {character.get('name')}")    
-    data = character.get("traits", "region", []) 
+    data = character.get("traits", [])
+    datar = character.get("region", [])
     if data:
         print("Traits: ")
         for trait in data:
             print(f"- {trait}")
+    if datar:
         print("Region: ")
-        for region in data:
+        for region in datar:
             print(f"- {region}")
+
+def display_region(region): 
+    print(f"Region: {region.get('name')}")    
+    datareg = region.get("description", []) 
+    if datareg:
+        print("Description: ")
+        for descr in datareg:
+            print(f"- {descr}")
 
 
 
 #Controls program flow: loads data, displays available characters, and handles user search loop.
 def main():
-    chars = check_json_files()
-    if chars is None:
-        print("Characters data missing. Exiting.")
+    lore_data = check_json_files()
+    if lore_data is None:
+        print("Data missing. Check correct files. Exiting.")
         return
+    
     
     print("------Characters------ ") #helps user see then seperated sections
 
-    for character in chars["characters"]: #goes through each character
+    for character in lore_data["characters"]: #goes through each character
         print(character.get("name")) #searches chars and gets each key value which is the names
 
     print("------Regions-------- ")    
-    for character in chars["characters"]: #goes through each character
-        print(character.get("region")) #
+    for region in lore_data["regions"]: #goes through each character
+        print(region.get("name")) #
 
     print("------'Quit' to exit program------ ") #helps user see then seperated sections
 
     while True:
         found = False 
-        user_input = input("Enter a Character: ").strip().lower() #moved lower here 
+        user_input = input("Enter a Character or Region: ").strip().lower() #moved lower here 
 
         if user_input == "quit": #added the choice to exit before searching through data
             print("Search ended")
             break #stops program breaks loop
 
-        result = search_characters(user_input, chars) #checks character data first if result is none goes to next function, passes chars
+        result = search_characters(user_input, lore_data) #checks character data first if result is none goes to next function, passes chars
         if result:
             found = True 
             display_character(result) #goes to next function
         
+        result = search_region(user_input, lore_data)
+        if result:
+            found = True
+            display_region(result)
 
         if not found: #prints below if found remains false
             print("Not Found.")
